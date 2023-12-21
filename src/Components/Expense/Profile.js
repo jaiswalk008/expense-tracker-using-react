d79ceb69962f14ea9b02d493b5266c82347b991a
Karan Jaiswal <jaiskaran008@gmail.com>
2023-12-22 01:55:27 +0530

    added get request for profile

--- src/Components/Expense/Profile.js
+++ src/Components/Expense/Profile.js
@@ -1,4 +1,4 @@
-import { useState ,useContext} from "react"
+import { useState ,useContext, useEffect} from "react"
 import Input from "../UI/Input"
 import './Expense.css'
 import axios from "axios";
@@ -7,9 +7,24 @@ import axios from "axios";
 const Profile = ({clickHander}) =>{
     const [name , setName] = useState('');
     const [url , setUrl] = useState('');
+    const [profileUpdated,setProfileUpdated] = useState(false);
+    const fetchProfile = async()=>{
+        try{
+            const res = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key='+process.env.REACT_APP_AUTH_KEY,
+        {idToken:localStorage.getItem('token')});
+        console.log(res.data.users[0]);
+        setName(res.data.users[0].displayName);
+        setUrl(res.data.users[0].photoUrl);
+        }
+        catch(err){console.log(err)}
+
+    }
     // const authCtx = useContext(AuthContext);
     const nameChangeHandler = (e) => {setName(e.target.value)}
     const urlChangeHandler = (e) => {setUrl(e.target.value)}
+    useEffect(() =>{
+        fetchProfile();
+    },[])
     const updateProfile = async (e) =>{
         e.preventDefault();
         const profileDetails={
@@ -21,6 +36,7 @@ const Profile = ({clickHander}) =>{
         const res = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:update?key='+process.env.REACT_APP_AUTH_KEY,
         profileDetails);
         console.log(res.data);
+        setProfileUpdated(true);
        } catch (error) {
             console.log(error);
        }
@@ -28,6 +44,7 @@ const Profile = ({clickHander}) =>{
     }
     return (
         <div className="profile-container">
+           {!profileUpdated &&  <>
             <div className="w-100">
                 <span>Contact Details</span>
                 <button className="btn float-end btn-danger" onClick={clickHander}> Cancel</button>
@@ -36,7 +53,8 @@ const Profile = ({clickHander}) =>{
                 <Input id="name" label="Full Name:" type="text" value={name} onChange={nameChangeHandler} />
                 <Input id="profile-photo" label="Profile Photo URL:" type="url" value={url} onChange={urlChangeHandler} />
                 <button id="update" className="btn">Update</button>
-            </form>
+            </form></>}
+            {profileUpdated && <h3>Profile Updated!!</h3>}
             <hr></hr>
         </div>
     )
