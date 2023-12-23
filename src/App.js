87d0c95e46036f3d0728fcc1800981ce8a773d5c
Karan Jaiswal <jaiskaran008@gmail.com>
2023-12-23 20:03:22 +0530

    added email verification

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css'
 import Expense from './Components/Expense/Expense';
 import Login from './Components/User/Login';
 import Signup from './Components/User/Signup';
-import { Route } from 'react-router-dom/cjs/react-router-dom.min';
+import { Route , Redirect} from 'react-router-dom/cjs/react-router-dom.min';
 function App() {
   
 
   return (
     <div className="App">
+      <Route path="/" exact>
+        <Redirect to="/login"></Redirect>
+      </Route>
       <Route path="/signup"><Signup/></Route>
-      <Route path="/login"><Login/></Route>
+      <Route path="/login" exact><Login/></Route>
       <Route path="/expense"><Expense/></Route>
       
     </div>
--- src/Components/Expense/Expense.js
+++ src/Components/Expense/Expense.js
@@ -1,12 +1,29 @@
 import { useState } from 'react';
 import './Expense.css';
 import Profile from './Profile';
+import axios from 'axios';
+
 const Expense = () =>{
     const [showProfileComponent , setShowProfileComponent] = useState(false);
-
+    const emailVerificationStatus = localStorage.getItem('emailVerified');
+    const token = localStorage.getItem('token');
+    const [error , setError] = useState('');
+    const [emailVerified , setEmailVerified] = useState(!!emailVerificationStatus);
     const completeProfileHandler = () =>{
         // console.log('hello');
         setShowProfileComponent(prevState => !prevState)
+    }
+    const verifyEmail =async () =>{
+        try {
+            const res = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key='+process.env.REACT_APP_AUTH_KEY,
+        {requestType:'VERIFY_EMAIL',
+    idToken:token});
+            setEmailVerified(true);
+            localStorage.setItem('emailVerified',true);
+        } catch (error) {
+            setError(error.response.data.error.message);
+        }
+        
     }
     return (
         <div>
@@ -16,6 +33,10 @@ const Expense = () =>{
             </nav>
             <hr></hr>
             {showProfileComponent && <Profile clickHander={completeProfileHandler}/>}
+           {!emailVerified && <div className='d-flex justify-content-center'>
+                {error.length>0 && <main>{error}</main>}
+                <p onClick={verifyEmail} id='verify-email'>Verify your email</p>
+            </div>}
         </div>
     )
 }
